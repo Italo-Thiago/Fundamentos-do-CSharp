a5148f388222c7cd083140aecc6417da1552b9ce
Italo-Thiago <italothiago220@gmail.com>
2024-03-07 16:57:30 -0300

    feat: add TwoMessages

---
+++ TwoMessages/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwoMessages
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("This is the first line.");
+      Console.WriteLine("This is the second line.");
+    }
+  }
+}
