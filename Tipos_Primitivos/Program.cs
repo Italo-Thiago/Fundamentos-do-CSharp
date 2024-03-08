da921b4dc9f3931337bd732019367a8371a144be
Italo-Thiago <italothiago220@gmail.com>
2024-03-07 21:36:23 -0300

    feat: add Tipos_Primitivos

---
+++ Tipos_Primitivos/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Resumo sobre Tipos Primitivos
+ * String => para palavras, frases ou qualquer dado alfanumérico para apresentação, não cálculo
+ * Char => para um caractere alfanumérico
+ * Int => para um número inteiro
+ * Decimal => para um número com um componente fracionário 
+ * Bool => para um valor true/false
+*/
+Console.WriteLine("Tipo String");
+Console.WriteLine("Italo Thiago");
+Console.WriteLine("Tipo Char");
+Console.WriteLine('I');
+Console.WriteLine("Tipo Int");
+Console.WriteLine(64);
+Console.WriteLine("Tipo Decimal");
+Console.WriteLine(3.14);
+Console.WriteLine("Tipo Booleano/Bool");
+Console.WriteLine(true);
+Console.WriteLine(false);
