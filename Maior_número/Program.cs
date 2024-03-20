b279137453feacde98c36eba060950ff37fed495
Italo Thiago <italothiago220@gmail.com>
2024-03-20 19:19:15 -0300

    feat: add Maior_número

---
+++ Maior_número/Program.cs
@@ -0,0 +1,7 @@
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+// A biblioteca Math vem por padrão com a System
+// Max e um método que compara duas variáveis e retorna a maior
+Console.WriteLine(largerValue);
