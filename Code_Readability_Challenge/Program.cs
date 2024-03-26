5c685f79ab2bf0c040f58be5a7c077d25f59e68c
Italo Thiago <italothiago220@gmail.com>
2024-03-25 22:37:18 -0300

    style: new arrangement of comments

---
+++ Code_Readability_Challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Convert the message into a char array
+Reverse the chars
+count the o's
+Convert it back to a string
+Print it out
+*/
+
+string message = "The quick brown fox jumped over the lazy dog.";
+
+char[] charMessage = message.ToCharArray();
+
+Array.Reverse(charMessage);
+int count = 0;
+
+foreach (char character in charMessage)
+{
+  if (character == 'o')
+  {
+    count++;
+  }
+}
+
+string newMessage = new string(charMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {x} times.");
