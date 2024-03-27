2acd01dfd6d20e76e2ecac4905b32bc75d0d1d5e
Italo Thiago <italothiago220@gmail.com>
2024-03-26 21:55:52 -0300

    feat: study 26/03'

---
+++ Arrays_and_loop_foreach/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+97 - 100   A+
+93 - 96    A
+90 - 92    A-
+87 - 89    B+
+83 - 86    B
+80 - 82    B-
+77 - 79    C+
+73 - 76    C
+70 - 72    C-
+67 - 69    D+
+63 - 66    D
+60 - 62    D-
+0  - 59    F
+*/
+
+
+// initialize variables - graded assignments
+int currentAssignment = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
+
+// Write the Report Header to the console
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia") 
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    
+    else if (currentStudent == "Logan")
+        studentScores = loganScores; 
+
+    // Initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initizalize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignment;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+}
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
